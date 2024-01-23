26cb49bb8bfbf91f994b77f75c92979d7a898c01
perazabg <96875802+perazabg@users.noreply.github.com>
2024-01-22 20:30:30 -0800

    nearly finished. just need to fix the read status button behavior

--- main.js
+++ main.js
@@ -1,41 +1,72 @@
-const myLibrary = [
-  {
-    title: "title",
-    author: "author",
-    pages: 69,
-    read: false
-  }
-];
-const openBtn = document.getElementById('openBtn');
-const dialog = document.getElementById('dialog');
-const closeBtn = document.getElementById('closeBtn')
 
-//show dialog via button
-openBtn.addEventListener('click', () => {
+let myLibrary = [];
+
+const dialog = document.getElementById("dialog");
+const closeBtn = document.getElementById("closeBtn");
+const form = document.getElementById("newBookForm");
+const newBookBtn = document.getElementById("newBookBtn");
+
+newBookBtn.addEventListener('click', () => {
+  form.reset();
   dialog.showModal();
 })
 
-closeBtn.addEventListener('click', () => {
+document.getElementById("newBookForm").addEventListener("submit", event => {
+  event.preventDefault();
+  addBook();
   dialog.close();
 })
-// book constructor 
-function book(title, author, pages, read){
+
+function Book(title, author, pages, read){
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 }
 
-function  addBook(){
-  // need to get book info first 
-  let title = $titleInput.value;
-  let author = $authorInput.value;
-  let pages = $pagesInput.value;
-  let read = getReadValue();
-  let newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
+Book.prototype.toggleRead = function(){
+  this.read = !this.read;
 }
 
-function getBookInfo(){
-  
+function toggleRead(index){
+  myLibrary[index].toggleRead;
+  renderLibrary();
+}
+
+function addBook(){
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").checked;
+  let newBook = new Book(title,author,pages,read);
+  myLibrary.push(newBook);
+  renderLibrary();
+}
+
+function removeBook(index){
+  myLibrary.splice(index, 1);
+  renderLibrary();
+}
+
+function renderLibrary(){
+  let bookshelf = document.getElementById("bookshelf");
+  bookshelf.innerHTML = "";
+  for(let i=0; i<myLibrary.length; i++){
+    let book = myLibrary[i];
+    let bookElement = document.createElement("div");
+    bookElement.setAttribute("class", "bookCard");
+    bookElement.innerHTML = `
+      <div class="cardHeader">
+        <h3 class="title">${book.title}</h3>
+        <h5 class="auther">${book.author}</h5>
+      </div>
+      <div class="cardBody">
+        <p>${book.pages}</p>
+        <p class="readStatus">${book.read ? "Read" : "Not Read Yet"}</p>
+        <button class="removeBtn" onclick="removeBook(${i})">Remove</button>
+        <button class="toggleReadBtn" onclick="toggleRead(${i})">Toggle Read</button>
+      </div>
+      `;
+    bookshelf.appendChild(bookElement);
+  }
 }
